9a139d386c288b13ea917a012afcdc3e1dba7a72
Mr_LS <82951310+Mr-cool08@users.noreply.github.com>
2025-08-30 00:11:45 +0200

    Serve wsgi app on port 80

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
+import os
+
 from main import app as application
 
 if __name__ == "__main__":
     # This allows running the app with `python wsgi.py`
-    application.run()
+    application.run(host="0.0.0.0", port=int(os.getenv("PORT", 80)))
