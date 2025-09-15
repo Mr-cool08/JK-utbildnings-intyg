22c8354d81cf4a1d67612353869f6732cee1ca75
liam suorsa <liam@suorsa.se>
2025-09-15 21:33:23 +0200

    more tests

--- tests/test_hash_value_deterministic.py
+++ tests/test_hash_value_deterministic.py
@@ -1,7 +1,7 @@
 import pytest
 from functions import hash_value
 
-@pytest.mark.parametrize('value', [f'value{i}' for i in range(100)])
+@pytest.mark.parametrize('value', [f'value{i}' for i in range(200)])
 def test_hash_value_deterministic(value):
     """Ensure hash_value returns consistent hash for the same input."""
     assert hash_value(value) == hash_value(value)
