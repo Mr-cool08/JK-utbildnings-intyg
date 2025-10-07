0696716f89ded5d1eb44deb2880cc49f332403be
liam suorsa <liam@suorsa.se>
2025-10-07 17:00:34 +0200

    fixed bug

--- app.py
+++ app.py
@@ -1001,7 +1001,7 @@ def admin_send_password_reset():
     try:
         token = functions.create_password_reset_token(normalized_personnummer, email)
     except ValueError as exc:
-        logger.exception(f"Misslyckades att skapa återställningstoken ({ValueError)}")
+        logger.exception(f"Misslyckades att skapa återställningstoken: {exc}")
         return jsonify({'status': 'error', 'message': 'Kunde inte skapa återställning.'}), 404
     except Exception as e:
         logger.exception(f"Misslyckades att skapa återställningstoken: {e}")
